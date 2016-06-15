e35596a30a7b6dc0d8781189fefa07e742b9c744
Kenneth Korcal <kakorcal@gmail.com>
2016-06-15 16:08:33 -0700

    home page styled

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const _ = require('lodash');
 
 router.get('/', (req, res)=>{
   knex.select([
     'u.id', 'u.username', 'p.id as photo_id', 'p.name as photo_name', 'p.url', 'p.date'
   ]).from('users as u')
   .join('photos as p', 'u.id', 'p.user_id')
-  .orderBy('u.id').then(users=>{
+  .orderBy('p.id').then(data=>{
+    const users = _.map(data, (cur)=>{
+      const mmddyear = new Date().toString().match(/[a-zA-Z]{3} \d{2} \d{4}/)[0];
+      return _.assignIn(cur, {date: mmddyear});
+    });
     res.render('home', {users});
   });
 });
--- seeds/USERS_PHOTOS_SEED.js
+++ seeds/USERS_PHOTOS_SEED.js
@@ -1,18 +1,23 @@
 exports.seed = function(knex, Promise) {
   return knex('users').del().then(()=>{
     return knex('users').insert([
-      {username: 'steve'},   
-      {username: 'andrew'},   
-      {username: 'uthra'},   
-      {username: 'parker'},   
+      {username: 'Steve'},   
+      {username: 'Andrew'},   
+      {username: 'Uthra'},   
+      {username: 'Parker'},   
     ]).returning('id');
   }).then(ids=>{
     return knex('photos').insert([
-      {name: 'blah1', url: 'dog1.jpg', date: new Date(), user_id: ids[0]},
-      {name: 'blah2', url: 'dog2.jpg', date: new Date(), user_id: ids[0]},
-      {name: 'blah3', url: 'dog3.jpg', date: new Date(), user_id: ids[1]},
-      {name: 'blah4', url: 'dog4.jpg', date: new Date(), user_id: ids[1]},
+      {name: 'blah1blah1blah1', url: 'dog1.jpg', date: new Date(), user_id: ids[0]},
+      {name: 'blah2blah1', url: 'dog2.jpg', date: new Date(), user_id: ids[0]},
+      {name: 'blah3blah1', url: 'dog3.jpg', date: new Date(), user_id: ids[1]},
+      {name: 'blah4blah1', url: 'dog4.jpg', date: new Date(), user_id: ids[1]},
       {name: 'blah5', url: 'dog5.jpg', date: new Date(), user_id: ids[2]},
+      {name: 'blah7', url: 'dog7.jpg', date: new Date(), user_id: ids[1]},
+      {name: 'blah8', url: 'dog8.jpg', date: new Date(), user_id: ids[1]},
+      {name: 'blah9blah1', url: 'dog9.jpg', date: new Date(), user_id: ids[0]},
+      {name: 'blah10blah1blah1', url: 'dog10.jpg', date: new Date(), user_id: ids[2]},
+      {name: 'blah11', url: 'dog11.jpg', date: new Date(), user_id: ids[2]}
     ]);
   });
 };
