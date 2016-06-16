3162292d1841998d6d14823564da254a625d0d47
Kenneth Korcal <kakorcal@gmail.com>
2016-06-15 19:39:27 -0700

    can edit photos

--- routes/photos.js
+++ routes/photos.js
@@ -14,8 +14,26 @@ router.post('/', (req, res)=>{
   });
 });
 
+
 router.get('/new', (req, res)=>{
   res.render('./components/photos/new', {user_id: req.params.user_id});
+});
+
+router.put('/:id', (req, res)=>{
+  const photo = Object.assign(req.body.photo, {date: new Date()});
+  knex('photos').where('id', +req.params.id).update(photo).then(()=>{
+    res.redirect(`/users/${req.params.user_id}`);
+  });
+});
+
+// router.delete('/:id', (req, res)=>{
+//   knex('photos').
+// });
+
+router.get('/:id/edit', (req, res)=>{
+  knex('photos').where('id', req.params.id).first().then(photo=>{
+    res.render('./components/photos/edit', {photo});
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,4 @@ router.get('/:id', (req, res)=>{
   });
 });
 
-
-
 module.exports = router;
