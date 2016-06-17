f8a93f7f73183ea9a15aea4002b45fa77fe10846
Kenneth Korcal <kakorcal@gmail.com>
2016-06-16 20:32:45 -0700

    adding comments describing passport authentication

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,13 @@ const bcrypt = require('bcrypt');
 const SALT_WORK_FACTOR = 10;
 const _ = require('lodash');
 
+// This passport middleware searches req.body and grabs the username and plain-text password
 passport.use(new LocalStrategy({
   usernameField: 'user[username]',
   passwordField: 'user[password]',
   passReqToCallback: true
 },
+// This callback includes the username and password that has been specified in the object above.
   function onSubmit(req, username, password, done){
     console.log('LocalStrategy');
     console.log('username', username);
@@ -22,15 +24,14 @@ passport.use(new LocalStrategy({
       console.log('inside last tests');
       console.log('user', user);
       eval(require('locus'));
-      if(!user){
-        return done(null, false, {message: 'Incorrect Username.'});
-      }
-
+      if(!user) return done(null, false, {message: 'Incorrect Username.'});
+      
       bcrypt.compare(password, user.password, (err, res)=>{
         console.log('inside compare function');
         console.log('err', err);
         console.log('res', res);
         eval(require('locus'));
+        // Test if this error should be passed to the verify callback or just throw an error
         if(err) throw err;
         if(!res) {
           return done(null, false, {message: 'Incorrect Password.'});
@@ -44,22 +45,16 @@ passport.use(new LocalStrategy({
 
 passport.serializeUser(function(user, done){
   console.log('SERIALIZE');
-  console.log('user', user);
-  console.log('done', done);
-  eval(require('locus'));
   done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done){
   console.log('DESERIALIZE');
-  console.log('id', id);
-  console.log('done', done);
   eval(require('locus'));
   knex('users').where("id",id).first().then(user => {
     eval(require('locus'));
     done(null, user);
   }).catch(err => {
-    eval(require('locus'));
     done(err);
   });
 });
@@ -74,24 +69,36 @@ router.post('/new', (req, res, next)=>{
     bcrypt.hash(req.body.user.password, salt, (err, hash)=>{
       // Store hash in db
       const credentials = _.assign(_.omit(req.body.user, 'password'), {password: hash});
-      knex('users').insert(credentials, '*').then(([user])=>{
+      knex('users').insert(credentials).then(()=>{
         eval(require('locus'));
+        // When passport.authenticate is called it invokes the local strategy constructor
+        // the verify callback is invoked with different params depending on the control flow of the
+        // local strategy constructor
         passport.authenticate('local', (err, user, info)=>{
           console.log('Authenticate after creating account');
           console.log('Error', err);
           console.log('User', user);
           console.log('Info', info);
           eval(require('locus'));
+          // This line is from the example in the docs, so unsure of what middleware is 
+          // invoked next if err exists
           if(err) return next(err);
+          // TODO: Add flash message
           if(!user) return res.redirect('/auth/new');
+          // if user has successfully created an account or verified their credentials
+          // call the logIn method. Not sure why it is attached to the req object and 
+          // not attached as a passport method. 
+          // When logIn is invoked, user is passed to serializeUser (if creating an account or 
+          // if they have an account and they are trying to login) and deserializeUser (this method 
+          // is called everytime to check if the stamp, aka session still exists)
           req.logIn(user, err=>{
+            // This error first callback is invoked only after serializing and deserializing
             if(err) return next(err);
             return res.redirect('/users');
           });
         })(req, res, next);
       }).catch(err=>{
-        eval(require('locus'));
-        res.redirect('/fail');
+        res.redirect('/error');
       });
     });
   });
